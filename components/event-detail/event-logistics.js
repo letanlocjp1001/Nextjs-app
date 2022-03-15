c01048d11c6be5280a429d209fcb9a99ebd22114
LE TAN LOC <letanlocjp1001@gmail.com>
2022-03-15 17:46:07 +0900

    update event

---
+++ components/event-detail/event-logistics.js
@@ -0,0 +1,33 @@
+import { FaRegCalendarAlt, FaMapMarkerAlt } from 'react-icons/fa'
+
+import LogisticsItem from './logistics-item'
+import classes from './event-logistics.module.css'
+
+function EventLogistics(props) {
+  const { date, address, image, imageAlt } = props
+
+  const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  })
+  const addressText = address.replace(', ', '\n')
+
+  return (
+    <section className={classes.logistics}>
+      <div className={classes.image}>
+        <img src={`/${image}`} alt={imageAlt} />
+      </div>
+      <ul className={classes.list}>
+        <LogisticsItem icon={FaRegCalendarAlt}>
+          <time>{humanReadableDate}</time>
+        </LogisticsItem>
+        <LogisticsItem icon={FaMapMarkerAlt}>
+          <address>{addressText}</address>
+        </LogisticsItem>
+      </ul>
+    </section>
+  )
+}
+
+export default EventLogistics
