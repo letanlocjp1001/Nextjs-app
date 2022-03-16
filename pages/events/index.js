2afbb5f177273ff93cce60a4eb29ce724f68c364
LE TAN LOC <letanlocjp1001@gmail.com>
2022-03-16 23:55:54 +0900

    update

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,16 @@
+import { Fragment } from 'react'
 import { getAllEvents } from '../../dummy-data'
 import EventList from '../../components/events/EventList'
+import EventsSearch from '../../components/events/EventsSearch'
 
 function AllEventsPage() {
   const events = getAllEvents()
 
   return (
-    <div>
+    <Fragment>
+      <EventsSearch />
       <EventList items={events} />
-    </div>
+    </Fragment>
   )
 }
 export default AllEventsPage
